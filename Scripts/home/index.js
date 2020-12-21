9c2381591d2d4fad78aff4e21d932a000ef0c7c7
Javier Encina <36193725+encinafr@users.noreply.github.com>
2020-12-21 09:56:22 -0300

    Initial commit

---
+++ Scripts/home/index.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+
+
+    $('#btn-submit').click(() => {
+        submit();
+    })
+
+});
+
+
+function submit() {
+
+    var model = {
+        Name: $("#name").val(),
+        PhoneNumber: $("#phoneNumber").val(),
+        Email: $("#email").val(),
+        Message: $("#message").val()
+    };
+
+    if (validate()) {
+
+        $.ajax({
+            url: '/Home/submit',
+            type: 'POST',
+            dataType: "json",
+            traditional: true,
+            data: model,
+            success: function (response) {
+
+                if (response.success) {
+
+                    alert(response.msg);
+                }
+                else {
+                    alert(response.msg);
+                }
+            }
+        });
+    }
+}
+
+function validate(){
+    return true;
+}
